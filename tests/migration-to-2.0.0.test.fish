aa29476bcc745adf5626da0dfa78abfdbf0daba2
Edouard Lopez <contact@edouard-lopez.com>
2019-05-23 04:54:10 +0200

    fix docker permissions issue due to mounting volume
    see: https://github.com/moby/moby/issues/7198

--- tests/migration-to-2.0.0.test.fish
+++ tests/migration-to-2.0.0.test.fish
@@ -1,18 +1,18 @@
 set --global DIRNAME $current_dirname
 
 function setup
-    cp $current_dirname/fixtures/{config.mock.fish,config.fish}
+    cp tests/fixtures/{config.mock.fish,config.fish}
 end
 
 function teardown
-    rm $current_dirname/fixtures/config.fish
+    rm tests/fixtures/config.fish
 end
 
 @test "migrate all variables" (
-    set file_to_migrate $current_dirname/fixtures/config.fish  # created during 'setup'
+    set file_to_migrate tests/fixtures/config.fish  # created during 'setup'
 
-    fish $current_dirname/../tools/migration-to-2.0.0.fish $file_to_migrate 2>&1 >/dev/null
-    
-    diff -U 0 $current_dirname/fixtures/config.expected.fish $file_to_migrate
+    fish tools/migration-to-2.0.0.fish $file_to_migrate 2>&1 >/dev/null
+
+    diff -U 0 tests/fixtures/config.expected.fish $file_to_migrate
 ) $status -eq 0
 
